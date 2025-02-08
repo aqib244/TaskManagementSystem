c9cf9847e18d14b85bf3436fa8d081a4e07df52f
aqib244 <aqibm8383@gmail.com>
2025-02-08 11:49:07 +0500

    Update AuthProvider.jsx

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,43 +1,39 @@
-import React, { createContext, useState, useEffect } from 'react'
+import React, { createContext, useState } from 'react'
 import { get_data, set_data } from '../utils/localStorage'
+import { useEffect } from 'react'
+ export const AuthContext = createContext()
+const AuthProvider = ({children}) => {
 
-// Context banaya gaya hai jo authentication data ko manage karega
-export const AuthContext = createContext()
-
-const AuthProvider = ({ children }) => {
+  const [userData , setUserData] = useState(null)
   
-  // User data ko state mein store karna
-  const [userData, setUserData] = useState(null)
-
   useEffect(() => {
-    // Local storage se data get karna
-    const data = get_data()
+   
+const data = get_data()
 
-    // Check karna agar data empty hai
-    const isDataEmpty = Object.values(data).every((value) => {
-      if (value === null) {
-        return true
-      }
-    });
 
-    // Agar data null ya empty ho to local storage ko update karna
-    if (data === null || isDataEmpty) {
-      set_data() // Default data set karna
-      const data = get_data() // Phir se updated data lena
-      setUserData(data) // State update karna
-    }
 
-    // Jo bhi data hai, usko state mein store karna
-    setUserData(data)
+  const isDataEmpty = Object.values(data).every((value)=> {if(value === null){
+    return true
+  }});
 
-  }, []) // Ye effect sirf ek dafa chalega component mount hone par
+if(data === null || isDataEmpty){
+  set_data()
+  const data = get_data()
+  setUserData(data)
+
+}
+setUserData(data)
+
+ 
   
+  
+ }, [])
+ 
   return (
     <div>
-      {/* Context Provider jo children components ko userData provide karega */}
-      <AuthContext.Provider value={userData}>
-        {children}
-      </AuthContext.Provider>
+     <AuthContext.Provider value={userData}>
+{children}
+     </AuthContext.Provider>
     </div>
   )
 }
